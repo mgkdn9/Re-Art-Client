f10415956196b2db471bf02d1156099b08e9842a
Mike Kohlberg <mkohlberg95@gmail.com>
2025-07-31 16:51:52 -0600

    fix 404

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Re-Art-Client">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
